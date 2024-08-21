1ff269f8948bf41e287080c9175ac3603b7cf05d
Andre Stovall <ohheyandrecodes@fastmail.com>
2024-08-21 15:13:16 -0700

    fixed determine icon function, successfully connected to database

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let entry = "electronic custom distributors #ltl bds /tomorrow 6pm"
+let entry = "lightning av part two #sm bds /tomorrow 9:31am"
 import Sugar from "./node_modules/sugar-date/index.js";
 import { configDotenv } from "dotenv";
 import { Client } from "@notionhq/client";
@@ -11,7 +11,7 @@ function splitUpString(string){
     const customerName = capitalCase(string.split("#")[0])
     let tags = string.slice(tagIndex).split("/")[0]
     let date = string.slice(dateIndex)
-    return [customerName, tags.substring(1, tags.length), date.substring(1, date.length)]
+    return {customerName: customerName, tags: tags.substring(1, tags.length), date: date.substring(1, date.length)}
 }
 
 function capitalCase(string) {
@@ -23,4 +23,77 @@ function capitalCase(string) {
     return newString.join(" ")
 }
 
-console.log(process.env.NOTION_TOKEN)
+function determineIcon(str) {
+    if (str.includes("sm")) return "📦"
+    if (str.includes("ltl")) return "🚚"
+}
+
+const { tags } = splitUpString(entry)
+console.log(tags, typeof tags)
+console.log(determineIcon(tags))
+
+const notion = new Client({
+    auth: process.env.NOTION_TOKEN
+});
+
+async function getAllOrders() {
+    const response = await notion.databases.query({
+        database_id: process.env.DATABASE_ID
+    })
+    console.log(response.results)
+}
+
+const order = splitUpString(entry)
+
+function testingSomething(str) {
+    const { customerName: name, tags, date } = str
+    // let icon = determineIcon(tags)
+    let convertedDate = Sugar.Date.create(date)
+    console.log(name, tags, date, convertedDate)
+    return name
+}
+
+testingSomething(order)
+
+async function addOrder(str) {
+    const { customerName: name, tags, date } = str
+    let icon = determineIcon(tags)
+    let convertedDate = Sugar.Date.create(date)
+    // console.log(name, tags, date, icon, convertedDate)
+    const response = await notion.pages.create({
+        parent: {
+            type: "database_id",
+            database_id: process.env.DATABASE_ID
+        },
+        icon: {
+            type: "emoji",
+            emoji: icon
+        },
+        properties: {
+            Name: {
+                type: 'title',
+                title: [
+                    {
+                        type: 'text',
+                        text: {
+                            content: name
+                        }
+                    }
+                ],
+            },
+            "Planned Date": {
+                type: 'date',
+                date: {
+                    start: convertedDate
+                },
+            },
+        },
+    })
+    console.log(response.created_time)
+}
+
+// const results = getAllOrders()
+// // console.log(results)
+// console.log(results.properties)
+
+const created = addOrder(order)
